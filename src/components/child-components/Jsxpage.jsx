0ce1cbf9fb159a978f470527efe830016cce26ef
Rohit Ware <rohitware99@gmail.com>
2025-05-20 14:48:15 +0530

    child component created

---
+++ src/components/child-components/Jsxpage.jsx
@@ -0,0 +1,9 @@
+function Jsxpage(){
+    return(
+
+        <>
+        <h2>Jsx Works</h2>
+        </>
+    )
+}
+export default Jsxpage
---
+++ src/components/child-components/Props.jsx
@@ -0,0 +1,8 @@
+function Props(){
+    return(
+        <>
+        <h2>Props Works</h2>
+        </>
+    )
+}
+export default Props
